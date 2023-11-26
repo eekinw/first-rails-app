0cf04e6bab372606616b61478b4e531c8d045961
Ee Kin <wongeekinn@gmail.com>
2023-11-26 17:04:26 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
+
   
-  get "/articles", to: "articles#index"
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles do 
+    resources :comments
+  end
 end
